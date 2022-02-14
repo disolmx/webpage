9cba65377d61826a18ae44a49544912bd2a8d0c0
Edmundo Andrade <edmundo.andrade@meganucleo.mx>
2022-02-13 22:00:55 -0600

    changes

--- next.config.js
+++ next.config.js
@@ -36,18 +36,19 @@ const nextConfig = {
       },
     ],
   },
-  i18n: {
-    locales: ['en', 'fr', 'de'],
-    defaultLocale: 'en',
-  },
-  webpack(config, { dev, ...other }) {
-    if (!dev) {
-      // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
-      config.resolve.alias['@formatjs/icu-messageformat-parser'] =
-        '@formatjs/icu-messageformat-parser/no-parser'
-    }
-    return config
-  },
+  //i18n: {
+  //  locales: ['es',],
+  //  defaultLocale: 'es',
+  //  localeDetection: false,
+  //},
+  //webpack(config, { dev, ...other }) {
+  //  if (!dev) {
+  //    // https://formatjs.io/docs/guides/advanced-usage#react-intl-without-parser-40-smaller
+  //    config.resolve.alias['@formatjs/icu-messageformat-parser'] =
+  //      '@formatjs/icu-messageformat-parser/no-parser'
+  //  }
+  //  return config
+  //},
 };
 
 module.exports = nextConfig
